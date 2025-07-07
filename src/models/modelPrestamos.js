61a0c05f8106b7554c606536e86e49e02607e166
Valentina Contreras <valentinacr1309@gmail.com>
2025-07-06 19:45:06 -0500

    Model,Controller,Router

--- src/models/modelPrestamos.js
+++ src/models/modelPrestamos.js
@@ -1,1 +1,36 @@
-console.log("controller");
+import { match } from 'assert';
+import { Schema, model} from 'mongoose';
+
+const schemaPrestamos= new Schema({
+ numero_documento:{
+    type: String,
+    required:true,
+    trim:true,
+ },
+ titulo_libro:{
+      type: String,
+    required:true,
+    trim:true,
+ },
+ fecha_prestamo:{
+    type: Date,
+    required:true
+ },
+ fecha_devolucion:{
+     type: Date,
+    required:true
+ },
+ fecha_entrega:{
+     type: Date,
+    required:true
+ },
+ estado:{
+    type:String,
+    required:true,
+    enum:["prestado","devuelto","retrasado"],
+    default:"prestado",
+    trim:true
+ }
+});
+
+export default model ('Prestamos',schemaPrestamos);
--- src/routers/routerPrestamos.js
+++ src/routers/routerPrestamos.js
@@ -1,1 +1,13 @@
-console.log("router");
+import {Router} from 'express';
+import controllerPrestamos from '../controllers/controllerPrestamos.js';
+
+const routerPrestamos = Router();
+
+routerPrestamos.post('/', controllerPrestamos.createPrestamo);
+routerPrestamos.get('/:id',controllerPrestamos.readPrestamo);
+routerPrestamos.get('/',controllerPrestamos.readPrestamos);
+routerPrestamos.put('/:id',controllerPrestamos.updatePrestamos);
+routerPrestamos.delete('/:id',controllerPrestamos.deletePrestamos);
+
+
+export default routerPrestamos;
