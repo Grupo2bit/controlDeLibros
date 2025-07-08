b24ba1886cc887d7fb843216db0f707c8000b37b
sebastian webb <sebastiaanwebb@outlook.com>
2025-07-08 18:45:58 -0500

    tabla de multas del proyecto

---
+++ src/models/modelsMultas.js
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+
+const multaSchema = new Schema({
+  numero_documento: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  titulo_libro: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  monto: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  fecha_Prestamo: {
+    type: Date,
+    required: true
+  },
+  fecha_devolucion: {
+    type: Date,
+    required: true
+  },
+  fecha_Limite_Pago: {
+    type: Date,
+    required: true
+  },
+  estado: {
+    type: String,
+    required: true,
+    enum: ['pendiente', 'pagada'],
+    default: 'pendiente',
+    trim: true
+  }
+});
+
+export default model('Multa', multaSchema);
